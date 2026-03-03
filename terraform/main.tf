b2c6fefef36bb353ef209696854cf5a73c2f2a0c
rafatusa <piealrafat@gmail.com>
2026-03-02 19:28:03 -0500

    Update via DevOps Agent

--- terraform/main.tf
+++ terraform/main.tf
@@ -107,11 +107,12 @@ resource "aws_security_group" "sg" {
 }
 
 resource "aws_instance" "server" {
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = var.instance_type
-  key_name               = aws_key_pair.deployer.key_name
-  subnet_id              = tolist(data.aws_subnets.default.ids)[0]
-  vpc_security_group_ids = [aws_security_group.sg.id]
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.deployer.key_name
+  subnet_id                   = tolist(data.aws_subnets.default.ids)[0]
+  vpc_security_group_ids      = [aws_security_group.sg.id]
+  associate_public_ip_address = true
 
   root_block_device {
     volume_size = 20
