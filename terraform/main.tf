286aac7223a562361edc50d72cec6ecbc5e9ff3d
rafatusa <piealrafat@gmail.com>
2026-03-02 20:21:10 -0500

    Fix: terraform/main.tf (attempt 1)

--- terraform/main.tf
+++ terraform/main.tf
@@ -110,7 +110,6 @@ resource "aws_instance" "server" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   key_name                    = aws_key_pair.deployer.key_name
-  subnet_id                   = tolist(data.aws_subnets.default.ids)[0]
   vpc_security_group_ids      = [aws_security_group.sg.id]
   associate_public_ip_address = true
 
